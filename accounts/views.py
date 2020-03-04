0503f15cfa54215ba5a01e726b8c4b3b0463430f
cvdebeer <claudiadiebeer@gmail.com>
2020-03-04 21:04:02 +0000

    Create login/out & profile pages and update routes

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import StudentRegistrationForm
 
 
@@ -10,9 +11,15 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             email = username
-            messages.success(request, f'Account created for {username}!')
-            return redirect('register')
+            messages.success(
+                request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
 
     else:
         form = StudentRegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
