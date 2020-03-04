39dd391c2b082ddf7c1c2a5ee3b9e8637273b213
cvdebeer <claudiadiebeer@gmail.com>
2020-03-04 13:40:17 +0000

    create the Student Registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,9 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 class StudentRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, label="First Name")
     last_name = forms.CharField(max_length=100, label="Last Name")
-    username = forms.EmailField(label='Email')
+    email = forms.EmailField(label='Email')
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name',
+        fields = ['username', 'first_name', 'last_name', 'email',
                   'password1', 'password2']
