7dc523966e5e39f102dd94bdc0083db80b94a404
cvdebeer <claudiadiebeer@gmail.com>
2020-03-05 19:15:38 +0000

    fix testimonial and add code for resizing images

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from PIL import Image
 
 
 class User(AbstractUser):
@@ -9,7 +10,22 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
-    experience = models.TextField(name='Testimonial', blank=True)
+    experience = models.TextField(
+        name='testimonial', blank=True, editable=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    '''
+    Code below is from a tutorial by Corey Shafer to scale down images that are uploaded by the user -https://www.youtube.com/watch?v=CQ90L5jfldw 
+    '''
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
