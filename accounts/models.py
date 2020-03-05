2886c6811b84637ddf247aefe49268604ba64931
cvdebeer <claudiadiebeer@gmail.com>
2020-03-05 11:29:47 +0000

    Profile model and signals for auto-create

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    experience = models.TextField(name='Testimonial', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
