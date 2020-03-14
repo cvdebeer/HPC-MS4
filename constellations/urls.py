d3a564e8ab94207b64fa31ad4284371c6806d155
cvdebeer <claudiadiebeer@gmail.com>
2020-03-14 18:08:08 +0000

    add testimonials to the sidebar

--- constellations/urls.py
+++ constellations/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about-page'),
     path('courses/', views.courses, name='courses'),
     path('mailing_list/', views.mailing_list, name='mailing-list'),
+    path('testimonials/', views.student_testimonials, name='testimonials')
 ]
