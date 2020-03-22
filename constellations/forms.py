fb2e27355db887df7a71ee9ab9e44414f1626af8
cvdebeer <claudiadiebeer@gmail.com>
2020-03-22 19:40:54 +0000

    remove unused code

--- constellations/forms.py
+++ constellations/forms.py
@@ -3,10 +3,6 @@ from .models import MailingList
 
 
 class MailingListForm(ModelForm):
-    # full_name = forms.CharField(max_length=100, label='Full Name')
-    # email_address = forms.EmailField(label='Your Email Address')
-    # accept_tc = forms.BooleanField(label='Accept Terms and Conditions')
-
     class Meta:
         model = MailingList
         fields = ['full_name', 'email_address', 'Accept terms and conditions']
